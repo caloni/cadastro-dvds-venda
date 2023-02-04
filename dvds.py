3eeddce2b353c5cb152f63f20a55eda465f99616
Wanderley Caloni <wanderley.caloni@intelitrader.com.br>
2023-02-04 10:34:09 -0300

    #1 Create and insert items to database.

--- dvds.py
+++ dvds.py
@@ -1,11 +1,23 @@
-# hello.py
-
 from flask import Flask, jsonify, request
+import sqlite3
+import json
 
-
-endpoint = __name__
-app = Flask(endpoint)
 version = "alpha"
+endpoint = __name__
+dbFile = __name__ + ".sqlite3"
+
+app = Flask(endpoint)
+db = sqlite3.connect(dbFile, check_same_thread=False)
+
+def startDb():
+  sql = None
+  with open('dvds.sql', 'r', encoding='utf-8') as f:
+    sql = f.read()
+  print('sql:', sql)
+  db.execute(sql)
+
+startDb()
+
 
 helloHelp = """Hello from dvds app v. {version}!
 
@@ -18,7 +30,7 @@ using a POST method and a json object like this:
 
 Use case:
 <pre>
-curl -H "application/json" -d "@dvd.json" http://this-api-address/dvds
+curl -H "Content-Type: application/json" -d "@dvd.json" http://this-api-address/dvds
 dvd criado com sucesso!
 </pre>
 """
@@ -32,7 +44,17 @@ def hello():
   ret = helloHelp.format(version=version, endpoint=endpoint, jsonSample=jsonSample)
   return ret
 
-@app.route('/dvds', methods=['POST'])
+
+@app.route("/" + endpoint, methods=['POST'])
 def register_dvd():
-  return request.get_json(), 204
+  dvd = request.get_json()
+  columns = ['productTitle', 'images', 'qty', 'price', 'condition',
+    'movieTrailer', 'delivery', 'takeout', 'warranty', 'format',
+    'movieTitle', 'movieDirector', 'resolution', 'disks', 'audio',
+    'gender', 'company']
+  keys= tuple(dvd[c] for c in columns)
+  cur = db.cursor()
+  cur.execute('insert into dvds values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', keys)
+  db.commit()
+  return "dvd " + str(cur.lastrowid) + " criado com sucesso!", 201
 
